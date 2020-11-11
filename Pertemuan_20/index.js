1563e4b3b9348cb13227fe62892ccc76fe2c2c95
favianmulumbot <s11910055@student.unklab.ac.id>
2020-11-12 07:27:31 +0800

    New Repository Update

---
+++ Pertemuan_20/index.js
@@ -0,0 +1,54 @@
+// // variable
+
+// // let & const 
+// let umur = 25;
+// _umur = 26;
+// let nilai = 95.5;
+// let sudahmenikah = "true";
+// let nama = "john";
+// console.log(_umur);
+// console.log(nama);
+
+// const tahunlahir = 2018;
+// console.log(tahunlahir);
+
+// Operator
+
+// Aritmatic Operator (+,-,*,/)
+
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log (angka1 + angka2);
+
+// let umur = 30
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log ("Nama Saya " + namaDepan + " " + namaBelakang + ". Umur saya adalah "+ umur + "Tahun ");
+
+// Operasi Perbandingan
+
+// let angka1 = 10;
+// let angka2 = 9;
+
+// console.log( angka1 == angka2);
+
+//ternary operator
+// let angka = 5;
+// let hasil = angka < 0 ? "Negatif" : "Positif"
+// console.log(hasil);
+
+//Operator Logika
+// let kondisi1 = true;
+// let kondisi2 = true;
+// console.log (kondisi1 || kondisi2);
+
+// let angka = prompt ("Masukkan angka : ");
+// console.log ("angka yang dimasukkan adalah " + angka);
+
+// Exercise 7
+
+let berat = prompt("masukan Berat badan anda = kg");
+let tinggi = prompt("masukan Tinggi = cm");
+
+let bni = berat / (tinggi*tinggi);
+console.log("BMI = " + bni);
